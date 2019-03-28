2c7086349b1590c376b95ca4f9b3b012e762925a
kongdemin <kdm67129772>
2019-03-28 10:28:28 +0800

    加入iview

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Button type="primary">Primary</Button>
   </div>
 </template>
 
